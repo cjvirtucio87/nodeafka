74cd101fa49ec5b338197ae0aa4c921320581b79
Virtucio <VirtucioC@pragmatics.com>
2017-04-07 07:59:15 -0400

    refactor(server): server file
    - decoupled server interface from kafka-server implementation

--- src/kafka-server.ts
+++ src/kafka-server.ts
@@ -1,8 +1,3 @@
-/* 
-    --- WINDOWS ONLY ---
-    Change file extensions in constants to .sh for UNIX.
-*/
-
 import { runZookeeperServer, runKafkaServer } from './runners';
 
 const run = runZookeeperServer(runKafkaServer(() => console.log('Done')));
---
+++ src/server.ts
@@ -0,0 +1,8 @@
+/* 
+    --- WINDOWS ONLY ---
+    Change file extensions in constants to .sh for UNIX.
+*/
+
+import run from './kafka-server';
+
+run();
